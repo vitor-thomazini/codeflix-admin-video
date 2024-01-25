cd0c0e53ad84f5c89eebf5c248ddec7a6717496d
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-01-24 21:23:42 -0300

    feat: configuração base do repositorio com inicializador spring boot

---
+++ infrastructure/src/test/java/vitor/thomazini/codeflixadminvideo/MainApplication.java
@@ -0,0 +1,13 @@
+package vitor.thomazini.codeflixadminvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
