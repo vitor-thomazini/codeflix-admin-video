75cb45b496967c86cf51c821b7e19aea6f8d9472
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-01-26 10:05:19 -0300

    feat: adicionar configuração da aplicação principal do 'spring boot'

--- infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/MainApplication.java
+++ infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/MainApplication.java
@@ -2,12 +2,14 @@ package vitor.thomazini.codeflixadminvideo.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+import vitor.thomazini.codeflixadminvideo.infrastructure.configuration.WebServerConfig;
 
 @SpringBootApplication
 public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+		SpringApplication.run(WebServerConfig.class, args);
 	}
-
 }
---
+++ infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package vitor.thomazini.codeflixadminvideo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("vitor.thomazini.codeflixadminvideo")
+public class WebServerConfig {
+}
