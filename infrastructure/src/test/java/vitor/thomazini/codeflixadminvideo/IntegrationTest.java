f91bc846008bd5903eb38ffff1126086d281de91
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-01-26 10:06:16 -0300

    test: validar integração da camada de aplicação ao criar 'category'

---
+++ infrastructure/src/test/java/vitor/thomazini/codeflixadminvideo/IntegrationTest.java
@@ -0,0 +1,17 @@
+package vitor.thomazini.codeflixadminvideo;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import vitor.thomazini.codeflixadminvideo.infrastructure.configuration.WebServerConfig;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@SpringBootTest(classes = WebServerConfig.class)
+@ExtendWith(MySQLCleanUpExtension.class)
+public @interface IntegrationTest {
+}
---
+++ infrastructure/src/test/java/vitor/thomazini/codeflixadminvideo/application/create/CreateCategoryUseCaseIT.java
@@ -0,0 +1,133 @@
+package vitor.thomazini.codeflixadminvideo.application.create;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import vitor.thomazini.codeflixadminvideo.IntegrationTest;
+import vitor.thomazini.codeflixadminvideo.application.category.create.CreateCategoryCommand;
+import vitor.thomazini.codeflixadminvideo.application.category.create.CreateCategoryUseCase;
+import vitor.thomazini.codeflixadminvideo.domain.category.CategoryGateway;
+import vitor.thomazini.codeflixadminvideo.infrastructure.category.persistence.CategoryRepository;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class CreateCategoryUseCaseIT {
+
+    @Autowired
+    private CreateCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallCreateCategory_thenShouldReturnCategoryId() {
+        // Arrange
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        // Act
+        final var actualOutput = this.useCase.execute(command).get();
+
+        // Assert
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        final var actualCategory = categoryRepository.findById(actualOutput.id()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallCreateCategory_thenShouldReturnDomainException() {
+        // Arrange
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        // Act
+        final var notification = this.useCase.execute(command).getLeft();
+
+        // Assert
+        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        verify(categoryGateway, times(0)).create(any());
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveCategory_whenCallCreateCategory_thenShouldReturnInactiveCategoryId() {
+        // Arrange
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        // Act
+        final var actualOutput = this.useCase.execute(command).get();
+
+        // Assert
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualCategory = categoryRepository.findById(actualOutput.id()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsRandomException_thenShouldReturnAException() {
+        // Arrange
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        doThrow(new IllegalStateException(expectedErrorMessage))
+                .when(categoryGateway).create(any());
+
+        // Act
+        final var notification = this.useCase.execute(command).getLeft();
+
+        // Assert
+        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+    }
+}
