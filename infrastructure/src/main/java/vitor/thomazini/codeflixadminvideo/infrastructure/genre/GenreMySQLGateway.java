c29f920ec2aeaac7a8f165e80c14a7d13a1ddd6b
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-01-31 17:32:01 -0300

    feat: implementar regras de persistencia do 'genre-gateway'

--- infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/genre/GenreMySQLGateway.java
@@ -1,2 +1,85 @@
-package vitor.thomazini.codeflixadminvideo.infrastructure.genre;public class GenreMySQLGateway {
+package vitor.thomazini.codeflixadminvideo.infrastructure.genre;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import vitor.thomazini.codeflixadminvideo.domain.genre.Genre;
+import vitor.thomazini.codeflixadminvideo.domain.genre.GenreGateway;
+import vitor.thomazini.codeflixadminvideo.domain.genre.GenreId;
+import vitor.thomazini.codeflixadminvideo.domain.pagination.Pagination;
+import vitor.thomazini.codeflixadminvideo.domain.pagination.SearchQuery;
+import vitor.thomazini.codeflixadminvideo.infrastructure.genre.persistence.GenreJpaEntity;
+import vitor.thomazini.codeflixadminvideo.infrastructure.genre.persistence.GenreRepository;
+import vitor.thomazini.codeflixadminvideo.infrastructure.utils.SpecificationUtils;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.Specification.where;
+
+@Component
+public class GenreMySQLGateway implements GenreGateway {
+
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
+    @Override
+    public Genre create(final Genre genre) {
+        return save(genre);
+    }
+
+    @Override
+    public void deleteById(final GenreId genreId) {
+        final var id = genreId.value();
+        if (this.genreRepository.existsById(id)) {
+            this.genreRepository.deleteById(id);
+        }
+    }
+
+    @Override
+    public Optional<Genre> findById(final GenreId id) {
+        return this.genreRepository.findById(id.value())
+                .map(GenreJpaEntity::toAggregate);
+    }
+
+    @Override
+    public Genre update(final Genre genre) {
+        return save(genre);
+    }
+
+    @Override
+    public Pagination<Genre> findAll(SearchQuery query) {
+        final var page = PageRequest.of(
+                query.page(),
+                query.perPage(),
+                Sort.by(Sort.Direction.fromString(query.direction()), query.sort())
+        );
+
+        final var where = Optional.ofNullable(query.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResult = this.genreRepository.findAll(where(where), page);
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(GenreJpaEntity::toAggregate).toList()
+        );
+    }
+
+    private Genre save(Genre genre) {
+        return this.genreRepository.save(
+                GenreJpaEntity.from(genre)
+        ).toAggregate();
+    }
+
+    private Specification<GenreJpaEntity> assembleSpecification(final String terms) {
+        return SpecificationUtils.like("name", terms);
+    }
 }
--- infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/genre/persistence/GenreRepository.java
+++ infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/genre/persistence/GenreRepository.java
@@ -1,2 +1,11 @@
-package vitor.thomazini.codeflixadminvideo.infrastructure.genre.persistence;public interface GenreRepository {
+package vitor.thomazini.codeflixadminvideo.infrastructure.genre.persistence;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<GenreJpaEntity, String> {
+
+    Page<GenreJpaEntity> findAll(Specification<GenreJpaEntity> whereClause, Pageable page);
 }
