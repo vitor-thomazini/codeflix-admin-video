d0cfb70a2562944b2fe2145e4357d458695b5595
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-02-08 11:29:40 -0300

    test: validar serialização e deserialização dos models de 'cast member'

---
+++ infrastructure/src/test/java/vitor/thomazini/codeflixadminvideo/infrastructure/castmember/models/UpdateCastMemberRequestTest.java
@@ -0,0 +1,34 @@
+package vitor.thomazini.codeflixadminvideo.infrastructure.castmember.models;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import vitor.thomazini.codeflixadminvideo.Fixture;
+import vitor.thomazini.codeflixadminvideo.JacksonTest;
+
+@JacksonTest
+class UpdateCastMemberRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCastMemberRequest> json;
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMembers.type();
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "type": "%s"
+                }
+                """.formatted(expectedName, expectedType);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("type", expectedType);
+    }
+}
