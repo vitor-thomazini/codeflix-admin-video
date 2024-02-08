1933a78d573ffe216f7a9d6e987dccb867cedf3d
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2024-02-08 10:10:16 -0300

    feat: implementar 'gateway' do 'cast member'

---
+++ infrastructure/src/main/java/vitor/thomazini/codeflixadminvideo/infrastructure/castmember/CastMemberMySQLGateway.java
@@ -0,0 +1,96 @@
+package vitor.thomazini.codeflixadminvideo.infrastructure.castmember;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import vitor.thomazini.codeflixadminvideo.domain.castmember.CastMember;
+import vitor.thomazini.codeflixadminvideo.domain.castmember.CastMemberGateway;
+import vitor.thomazini.codeflixadminvideo.domain.castmember.CastMemberId;
+import vitor.thomazini.codeflixadminvideo.domain.pagination.Pagination;
+import vitor.thomazini.codeflixadminvideo.domain.pagination.SearchQuery;
+import vitor.thomazini.codeflixadminvideo.infrastructure.castmember.persistence.CastMemberJpaEntity;
+import vitor.thomazini.codeflixadminvideo.infrastructure.castmember.persistence.CastMemberRepository;
+import vitor.thomazini.codeflixadminvideo.infrastructure.utils.SpecificationUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+@Component
+public class CastMemberMySQLGateway implements CastMemberGateway {
+
+    private final CastMemberRepository castMemberRepository;
+
+    public CastMemberMySQLGateway(final CastMemberRepository castMemberRepository) {
+        this.castMemberRepository = Objects.requireNonNull(castMemberRepository);
+    }
+
+    @Override
+    public CastMember create(final CastMember aCastMember) {
+        return save(aCastMember);
+    }
+
+    @Override
+    public void deleteById(final CastMemberId aMemberId) {
+        final var anId = aMemberId.value();
+        if (this.castMemberRepository.existsById(anId)) {
+            this.castMemberRepository.deleteById(anId);
+        }
+    }
+
+    @Override
+    public Optional<CastMember> findById(final CastMemberId anId) {
+        return this.castMemberRepository.findById(anId.value())
+                .map(CastMemberJpaEntity::toAggregate);
+    }
+
+    @Override
+    public CastMember update(final CastMember aCastMember) {
+        return save(aCastMember);
+    }
+
+    @Override
+    public Pagination<CastMember> findAll(final SearchQuery aQuery) {
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var where = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResult =
+                this.castMemberRepository.findAll(where, page);
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(CastMemberJpaEntity::toAggregate).toList()
+        );
+    }
+
+    @Override
+    public List<CastMemberId> existsByIds(final Iterable<CastMemberId> castMemberIds) {
+        final var ids = StreamSupport.stream(castMemberIds.spliterator(), false)
+                .map(CastMemberId::value)
+                .toList();
+        return this.castMemberRepository.existsByIds(ids).stream()
+                .map(CastMemberId::from)
+                .toList();
+    }
+
+    private CastMember save(final CastMember aCastMember) {
+        return this.castMemberRepository.save(CastMemberJpaEntity.from(aCastMember))
+                .toAggregate();
+    }
+
+    private Specification<CastMemberJpaEntity> assembleSpecification(final String terms) {
+        return SpecificationUtils.like("name", terms);
+    }
+}
